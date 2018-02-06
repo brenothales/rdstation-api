b0f02dd413fabb953cb67dd8b391c7a04b5f28f9
Breno Thales <breno.thales@gmail.com>
2018-02-06 20:51:05 -0200

    configurantion ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,25 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  #Added per active admin install instructions
+config.action_mailer.default_url_options = { :host => 'https://rdstationapi.herokuapp.com/' }
+
+
+#These settings are for the sending out email for active admin and consequently the   devise mailer
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings =
+{
+
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'brenothales1@gmail.com',
+  :password           => '1@#thalesWeb4288'
+}
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
